50c9ea44bcb8267a49b68e53cfe2184a1f2b5462
Bruno Bernardo <bigsby@live.com>
2017-02-28 18:12:31 +0000

    Test go args and input

--- src/go/main.go
+++ src/go/main.go
@@ -2,9 +2,19 @@ package main
 
 import "os"
 import "fmt"
+import "bufio"
 
 func main(){
     args := os.Args[1:]
 
-    fmt.Println(args[0])
+    if len(args) == 1 {
+        fmt.Println(args[0])
+    } else {
+        fmt.Println("No args")
+    }
+
+    fmt.Println("Input something: ")
+    reader := bufio.NewReader(os.Stdin)
+    input, _, _ := reader.ReadLine()
+    fmt.Println(string(input))
 }

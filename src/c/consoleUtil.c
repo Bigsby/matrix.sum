c252b36958e54316a1003ff99cbb1f60636cc610
Bruno Bernardo <bruno@peacock.home>
2023-10-19 19:23:37 +0100

    c fix function signature

--- src/c/consoleUtil.c
+++ src/c/consoleUtil.c
@@ -4,8 +4,7 @@
 
 #include "./results.h"
 
-void *Prompt(char *question, char *result)
-{
+void Prompt(char *question, char *result) {
     printf("%s\n", question);
     fgets(result, 100, stdin);
 }
@@ -15,9 +14,9 @@ void DisplayMatrixResult(MatrixResult result) {
     sprintf(maxString, "%d", result.Side * result.Side);
     int maxLength = strlen(maxString) + 1;
     int length = maxLength > 2 ? maxLength : 2;
-    
+
     for (int row = 0; row < result.Side; row++) {
-        for (int column = 0; column < result.Side; column++){
+        for (int column = 0; column < result.Side; column++) {
             printf("%*d", length, result.Matrix[row][column]);
         }
         printf("\n");
